bc1ebf7f7a4a8d723dd8b12bb63a2daa2c97704d
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 18:56:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,24 +21,24 @@ class Artist
   end
 
 
-  # def self.find_or_create_by_name(name)
-  #   if self.find(name)
-  #     self.find(name)
-  #   else
-  #     self.create(name)
-  #   end
-  # end
-  #
-  # def self.find(name)
-  #   self.all.detect { |artist| artist.name == name }
-  #   artist.save
-  #   artist
-  # end
-  #
-  # def self.create(name)
-  #   artist = Artist.new(name)
-  #   artist.save
-  #   artist
-  # end
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+    artist.save
+    artist
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 
 end
