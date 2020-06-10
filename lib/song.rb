2f98b6a323a0b548a4249a3aee6e57a23d7244c4
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 19:48:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
   end
 
   def self.new_by_filename(file)
-    binding.pry
+    # binding.pry
     song_name = file.split(" - ")[1]
     artist = file.split(" - ")[0]
     song = self.new(song_name)
